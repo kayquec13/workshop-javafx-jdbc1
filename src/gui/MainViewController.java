ba54a9a7c141105c284f95229964ce246651c831
kayquec13 <kayquecbenatti@gmail.com>
2021-02-21 17:27:08 -0300

    Criado telas de registro e ajuda

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,13 +1,22 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -15,27 +24,50 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
-	
+	//Método que passa o nome da tela para para o LoadView
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
+	}
+
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			//FXMLLoader Carrega uma tela
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			VBox newVBox = loader.load();
+			//Pega a Scene principal
+			Scene mainScene = Main.getmainScene();
+			//Acessa a tag content
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			//Dentro da tag Content acessa o primeiro Children
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpa os Children da VBox MainView
+			mainVBox.getChildren().clear();		
+			// Adiciona o menu da MainView
+			mainVBox.getChildren().add(mainMenu);
+			// Adiciona a coleção de children da view que for chamada
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
