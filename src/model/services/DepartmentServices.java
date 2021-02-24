a8ed29128318d5097e1e85ca16009f494815d66b
kayquec13 <kayquecbenatti@gmail.com>
2021-02-24 00:39:50 -0300

    Foi adicionado a conexão com o banco de dados

--- src/model/services/DepartmentServices.java
+++ src/model/services/DepartmentServices.java
@@ -1,17 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentServices {
 
+	private DepartmentDao dao = DaoFactory.creaDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> list =  new ArrayList<>();
-		list.add(new Department(1, "Livros"));
-		list.add(new Department(2, "Computadores"));
-		list.add(new Department(3, "Eletronicos"));		
-		return list;		
+		return dao.findAll();
 	}
 }
